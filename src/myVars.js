235dda783e76f9f187a98e3ac85715b3fe32667c
galalkoro98 <galal.koro@gmail.com>
2023-01-13 23:29:48 +0100

    add doms element

---
+++ src/myVars.js
@@ -0,0 +1,9 @@
+// my vars
+
+export const searchForm = document.getElementById("search-form");
+export const searchInput = document.getElementById("search-input");
+export const searchSubmit = document.getElementById("search-submit");
+export const result = document.getElementById("result");
+export const url = `https://api.openai.com/v1/engines/davinci/completions`;
+export const searchTerm = searchInput.value;
+export const API_KEY = "here you need real api_key";
